c173971b345a867ec6baa01b9c3761673220870c
[diegohartmann] <[contato.hartmann@gmail.com]>
2022-08-03 20:46:06 -0300

    animation changes

--- js/modules/helpers.js
+++ js/modules/helpers.js
@@ -1,11 +1,10 @@
-
 export const valor = document.querySelector('header')
 
 function animar(){
     valor.classList.add('animarValor')
     setTimeout(()=>{
         valor.classList.remove('animarValor')
-    }, 200)
+    }, 150)
 }
 
 export function setarValor(val){ 
@@ -16,10 +15,10 @@ export function incrementarValor(val){
     valor.innerText += val 
     if(valor.innerText[0] === '0'){
         let _array = (valor.innerText).split('');
-        _array.shift() //remove o primero el
+        _array.shift()
         valor.innerText = _array.join(''); //string alterada
     }
     animar();
 }
 
-export function addClick(el, callBack){ el.addEventListener('click', callBack) }
+export const addClick = ( el, callBack ) =>  el.addEventListener( 'click', callBack )

aba58f98c1970b3b10d897b5f857990bef21d5d4
[diegohartmann] <[contato.hartmann@gmail.com]>
2022-08-03 21:01:09 -0300

    checkpoint

--- js/modules/teclado.js
+++ js/modules/teclado.js
@@ -20,30 +20,30 @@ const operacoes = [
 ]
 const calcular = [ '=', 'Enter', 'Space', 'Spacebar' ]
 
+// const limpar = ['Backspace']
 
-
+function animarBotao(teclaPressionada, nomeDoMetodo, callBack=(key)=>console.log(key)){
+    
+    //escrever esses caracteres
+    operacoes.includes(teclaPressionada) && callBack(teclaPressionada)
+    
+    //animar botao
+    getarBotoes()?.forEach(btn=>{
+        const podePressionar = (btn.getAttribute('data-val').includes(teclaPressionada))
+        podePressionar && btn.classList[nomeDoMetodo]("animarBotao")
+    })
+}
 export const teclado = () => {
 
     window.addEventListener('keypress', (e)=>{
         const myKey = `${e.key}`
-        if(operacoes.includes(myKey)){
-            getarBotoes()?.forEach(btn=>{
-                const _match_ = (myKey === btn.getAttribute('data-val'))
-                _match_ && btn.classList.add("animarBotao")
-            })
-        } 
+        animarBotao(myKey, 'add');
     })
 
     window.addEventListener('keyup', (e)=>{
         const myKey = `${e.key}`
-        if(operacoes.includes(myKey)){
-            incrementarValor(myKey)
-            getarBotoes()?.forEach(btn=>{
-                const match = (myKey === btn.getAttribute('data-val'))
-                match && btn.classList.remove("animarBotao")
-            })
-            return;
-        } 
+        console.log(myKey)
+        animarBotao(myKey, 'remove', incrementarValor);
         if(calcular.includes(myKey)){
             setarValor( eval(valor.innerText) );
             return;

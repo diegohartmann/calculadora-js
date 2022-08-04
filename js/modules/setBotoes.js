8680f876e9521f0cc738503cf1475eea6a2032a3
[diegohartmann] <[contato.hartmann@gmail.com]>
2022-08-03 21:54:37 -0300

    cancelar

--- js/modules/setBotoes.js
+++ js/modules/setBotoes.js
@@ -2,18 +2,17 @@ import { addClick, incrementarValor } from "./helpers.js"
 
 const get = (selector) => Array.from(document.querySelectorAll(selector))
 
-let botoes;
-export const getarBotoes = () => botoes ?? []
+let botoesParaAnimar;
+export const getarBotoesParaAnimar = () => botoesParaAnimar ?? []
 
 let botoesParaEscrever;
 export const getarbotoesParaEscrever = () => botoesParaEscrever ?? []
 
 
-
 export const setBotoes = () => {
-    botoes = get('.btn');
-    
+    botoesParaAnimar = get('.btn');
     botoesParaEscrever = [...get('.numero'), ...get('.operacao') ]
+    
     botoesParaEscrever.forEach( btn => {
         addClick(btn, ()=> {
             incrementarValor(btn.getAttribute('data-val'))
--- js/modules/teclado.js
+++ js/modules/teclado.js
@@ -1,43 +1,11 @@
 import { incrementarValor, setarValor, valor } from "./helpers.js"
-import { getarBotoes, getarbotoesParaEscrever } from "./setBotoes.js"
+import { getarBotoesParaAnimar, getarbotoesParaEscrever } from "./setBotoes.js"
 
-// const operacoes = [
-//     '0',
-//     '1',
-//     '2',
-//     '3',
-//     '4',
-//     '5',
-//     '6',
-//     '7',
-//     '8',
-//     '9',
-//     '+',
-//     '-',
-//     '/',
-//     '*',
-//     '.',
-// ]
-// const calcular = [ '=', 'Enter', 'Space', 'Spacebar' ]
-
-// const limpar = ['Backspace']
-
-// function animarBotao(teclaPressionada, nomeDoMetodo, callBack=(key)=>console.log(key)){
-//     //escrever esses caracteres
-//     operacoes.includes(teclaPressionada) && callBack(teclaPressionada)
-    
-//     //animar botao
-//     getarBotoes()?.forEach(btn=>{
-//         const podePressionar = (btn.getAttribute('data-val').includes(teclaPressionada))
-//         podePressionar && btn.classList[nomeDoMetodo]("animarBotao")
-//     })
-// }
-
-const pegarBotaoPeloEvento = e => getarBotoes().find( btn => btn.getAttribute('data-inputs').split("#").includes(e.key.toLowerCase()) ) 
+const pegarBotaoPeloEvento = e => getarBotoesParaAnimar().find( btn => btn.getAttribute('data-inputs').split("#").includes(e.key.toLowerCase()) ) 
 
 export const teclado = () => {
 
-    window.addEventListener('keypress', e => {
+    window.addEventListener('keydown', e => {
         const botaoApertado = pegarBotaoPeloEvento(e) 
         botaoApertado?.classList.add("animarBotao")
         console.log(e.key)
@@ -51,25 +19,8 @@ export const teclado = () => {
             incrementarValor(botaoSolto.getAttribute('data-val'));
         }else if(botaoSolto?.getAttribute('data-inputs').split('#').includes('enter')){
             setarValor( eval(valor.innerText) );
+        }else if(e.key === 'Escape'){
+            setarValor('0')
         }
     })
-
-    // window.addEventListener('keypress', (e)=>{
-    //     const myKey = `${e.key}`
-    //     animarBotao(myKey, 'add');
-    // })
-
-    // window.addEventListener('keyup', (e)=>{
-    //     const myKey = `${e.key}`
-    //     console.log(myKey)
-    //     animarBotao(myKey, 'remove', incrementarValor);
-    //     if(calcular.includes(myKey)){
-    //         setarValor( eval(valor.innerText) );
-    //         return;
-    //     }
-    //     switch(myKey){
-    //         case ',': incrementarValor('.'); break;
-    //         case 'Escape': setarValor('0'); break;
-    //     }
-    // })
 }

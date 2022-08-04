56c6730ab0dab63606598adbf98ea8d0f8e4925f
[diegohartmann] <[contato.hartmann@gmail.com]>
2022-08-04 10:09:10 -0300

    color.js commented

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ import { setBotoes } from './modules/setBotoes.js'
 import { setCalcular } from './modules/setCalcular.js'
 import { setHeader } from './modules/setHeader.js'
 import { teclado } from './modules/teclado.js'
-import { color } from './modules/color.js'
+// import { color } from './modules/color.js'
 
 window.addEventListener('load', ()=>{
-    color();
+    // color();
     setHeader();
     setBotoes();
     setCalcular();
